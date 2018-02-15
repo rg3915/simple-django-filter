e816c9330b9ab5625bbb66f5a22682e23c44dbdd
rg3915 <rg3915@yahoo.com.br>
2018-02-15 20:12:31 -0200

    year_joined lookup

--- myproject/core/filters.py
+++ myproject/core/filters.py
@@ -3,6 +3,11 @@ import django_filters
 
 
 class UserFilter(django_filters.FilterSet):
+    first_name = django_filters.CharFilter(lookup_expr='icontains')
+    year_joined = django_filters.NumberFilter(
+        name='date_joined',
+        lookup_expr='year'
+    )
 
     class Meta:
         model = User
