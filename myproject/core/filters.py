77a1927677417dfc1d533beeb8bc6faa49f86ea0
rg3915 <rg3915@yahoo.com.br>
2018-02-15 20:27:36 -0200

    Using dict and add groups

--- myproject/core/filters.py
+++ myproject/core/filters.py
@@ -3,20 +3,13 @@ import django_filters
 
 
 class UserFilter(django_filters.FilterSet):
-    first_name = django_filters.CharFilter(lookup_expr='icontains')
-    year_joined = django_filters.NumberFilter(
-        name='date_joined',
-        lookup_expr='year'
-    )
-    year_joined__gt = django_filters.NumberFilter(
-        name='date_joined',
-        lookup_expr='year__gt'
-    )
-    year_joined__lt = django_filters.NumberFilter(
-        name='date_joined',
-        lookup_expr='year__lt'
-    )
 
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', ]
+        fields = {
+            'username': ['exact', ],
+            'first_name': ['icontains', ],
+            'last_name': ['exact', ],
+            'date_joined': ['year', 'year__gt', 'year__lt', ],
+            'groups': ['exact', ]
+        }
