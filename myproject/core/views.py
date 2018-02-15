ec324939a8c459fbd6a105a366cfc05d967a06c0
rg3915 <rg3915@yahoo.com.br>
2018-02-15 19:53:33 -0200

    Filter it work

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from .filters import UserFilter
 
-# Create your views here.
+
+def search(request):
+    user_list = User.objects.all()
+    user_filter = UserFilter(request.GET, queryset=user_list)
+    return render(request, 'core/user_list.html', {'filter': user_filter})
