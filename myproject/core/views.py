db7046b8b6fc83b5f2fe7b004f9974fd0a005c60
rg3915 <rg3915@yahoo.com.br>
2018-02-15 20:01:04 -0200

    Search with CBV

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
+from django_filters.views import FilterView
 from .filters import UserFilter
 
 
@@ -7,3 +8,9 @@ def search(request):
     user_list = User.objects.all()
     user_filter = UserFilter(request.GET, queryset=user_list)
     return render(request, 'core/user_list.html', {'filter': user_filter})
+
+
+search_cbv = FilterView.as_view(
+    filterset_class=UserFilter,
+    template_name='core/user_list.html',
+)
--- myproject/urls.py
+++ myproject/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from myproject.core import views
 
+
 urlpatterns = [
     path('search/', views.search, name='search'),
+    path('search/cbv/', views.search_cbv, name='search_cbv'),
     path('admin/', admin.site.urls),
 ]
